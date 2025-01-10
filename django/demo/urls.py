c42591c88d0413424649bf474ea0aaeadc981635
Venkat Kumar <isvenkatkumar@gmail.com>
2025-01-10 07:24:44 +0530

    tech(nginx/): code changes for nginx load balancing between two containers and cache ignore based on Vary headers

--- django/demo/urls.py
+++ django/demo/urls.py
@@ -1,10 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from demoapp import views
+import os
 
+DYNAMIC_TEMPLATE_NAME = f"split{os.environ.get('CONTAINER_VERSION', 1)}.html"
+print(DYNAMIC_TEMPLATE_NAME, os.environ.get('CONTAINER_VERSION'))
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('p1/',TemplateView.as_view(template_name='p1.html')),
-    path('p2/',TemplateView.as_view(template_name='p2.html')),
+    path('p1/', views.homepage),
+    path('p2/', TemplateView.as_view(template_name="p2.html")),
+    path('split_test', TemplateView.as_view(template_name=DYNAMIC_TEMPLATE_NAME)),
 ]
